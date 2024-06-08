d53e359b3fbb5c494e19fb132d7faf6f729f0580
Justin Li <justinnhli@gmail.com>
2024-06-08 07:53:35 -0700

    [matrix] rename Matrix subtypes

--- dumpy/matrix.py
+++ dumpy/matrix.py
@@ -12,56 +12,56 @@ EPSILON = 0.00001
 class Matrix: # pylint: disable = too-many-public-methods
     """A matrix.
 
-    >>> Point(3, -2, 5) + Vector(-2, 3, 1)
-    Point(1, 1, 6)
+    >>> Point3D(3, -2, 5) + Vector3D(-2, 3, 1)
+    Point3D(1, 1, 6)
 
-    >>> Point(3, 2, 1) - Point(5, 6, 7)
-    Vector(-2, -4, -6)
+    >>> Point3D(3, 2, 1) - Point3D(5, 6, 7)
+    Vector3D(-2, -4, -6)
 
-    >>> Point(3, 2, 1) - Vector(5, 6, 7)
-    Point(-2, -4, -6)
+    >>> Point3D(3, 2, 1) - Vector3D(5, 6, 7)
+    Point3D(-2, -4, -6)
 
-    >>> Vector(3, 2, 1) - Vector(5, 6, 7)
-    Vector(-2, -4, -6)
+    >>> Vector3D(3, 2, 1) - Vector3D(5, 6, 7)
+    Vector3D(-2, -4, -6)
 
-    >>> Tuple4(1, -2, 3, -4) * 3.5 == Tuple4(3.5, -7, 10.5, -14)
+    >>> Matrix([[1, -2, 3, -4]]) * 3.5 == Matrix([[3.5, -7, 10.5, -14]])
     True
 
-    >>> 0.5 * Tuple4(1, -2, 3, -4) == Tuple4(0.5, -1, 1.5, -2)
+    >>> 0.5 * Matrix([[1, -2, 3, -4]]) == Matrix([[0.5, -1, 1.5, -2]])
     True
 
-    >>> Tuple4(1, -2, 3, -4) / 2 == Tuple4(0.5, -1, 1.5, -2)
+    >>> Matrix([[1, -2, 3, -4]]) / 2 == Matrix([[0.5, -1, 1.5, -2]])
     True
 
-    >>> Vector(1, 0, 0).magnitude == 1
+    >>> Vector3D(1, 0, 0).magnitude == 1
     True
 
-    >>> Vector(0, 1, 0).magnitude == 1
+    >>> Vector3D(0, 1, 0).magnitude == 1
     True
 
-    >>> Vector(0, 0, 1).magnitude == 1
+    >>> Vector3D(0, 0, 1).magnitude == 1
     True
 
-    >>> Vector(1, 2, 3).magnitude == sqrt(14)
+    >>> Vector3D(1, 2, 3).magnitude == sqrt(14)
     True
 
-    >>> Vector(-1, -2, -3).magnitude == sqrt(14)
+    >>> Vector3D(-1, -2, -3).magnitude == sqrt(14)
     True
 
-    >>> Vector(4, 0, 0).normalize() == Vector(1, 0, 0)
+    >>> Vector3D(4, 0, 0).normalize() == Vector3D(1, 0, 0)
     True
 
-    >>> Vector(1, 2, 3).normalize().magnitude == 1
+    >>> Vector3D(1, 2, 3).normalize().magnitude == 1
     True
 
-    >>> Vector(1, 2, 3).dot(Vector(2, 3, 4))
+    >>> Vector3D(1, 2, 3).dot(Vector3D(2, 3, 4))
     20
 
-    >>> Vector(1, 2, 3).cross(Vector(2, 3, 4))
-    Vector(-1, 2, -1)
+    >>> Vector3D(1, 2, 3).cross(Vector3D(2, 3, 4))
+    Vector3D(-1, 2, -1)
 
-    >>> Vector(2, 3, 4).cross(Vector(1, 2, 3))
-    Vector(1, -2, 1)
+    >>> Vector3D(2, 3, 4).cross(Vector3D(1, 2, 3))
+    Vector3D(1, -2, 1)
 
     >>> m1 = Matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
     >>> m2 = Matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
@@ -80,9 +80,8 @@ class Matrix: # pylint: disable = too-many-public-methods
     True
 
     >>> m1 = Matrix([[1, 2, 3, 4], [2, 4, 4, 2], [8, 6, 4, 1], [0, 0, 0, 1]])
-    >>> t = Tuple4(1, 2, 3, 1)
-    >>> m1 @ t
-    Point(18, 24, 33)
+    >>> m1 @ Point3D(1, 2, 3)
+    Point3D(18, 24, 33)
 
     >>> m1 = Matrix([[0, 1, 2, 4], [1, 2, 4, 8], [2, 4, 8, 16], [4, 8, 16, 32]])
     >>> m2 = identity()
@@ -207,11 +206,11 @@ class Matrix: # pylint: disable = too-many-public-methods
         if self.is_tuple:
             vals = [str(abs(i) if i == 0 else i) for i in self.rows[0]]
             if self.is_vector:
-                return f'Vector({", ".join(vals[:-1])})'
+                return f'Vector3D({", ".join(vals[:-1])})'
             elif self.is_point:
-                return f'Point({", ".join(vals[:-1])})'
+                return f'Point3D({", ".join(vals[:-1])})'
             else:
-                return f'Tuple4({", ".join(vals)})'
+                return f'Tuple({", ".join(vals)})'
         else:
             return f'Matrix({str(self.rows)})'
 
@@ -273,7 +272,7 @@ class Matrix: # pylint: disable = too-many-public-methods
     def normalize(self):
         # type: () -> Matrix
         magnitude = self.magnitude
-        return Vector(self.x / magnitude, self.y / magnitude, self.z / magnitude)
+        return Vector3D(self.x / magnitude, self.y / magnitude, self.z / magnitude)
 
     def reflect(self, other):
         # type: (Matrix) -> Matrix
@@ -286,7 +285,7 @@ class Matrix: # pylint: disable = too-many-public-methods
 
     def cross(self, other):
         # type: (Matrix) -> Matrix
-        return Vector(
+        return Vector3D(
             self.y * other.z - self.z * other.y,
             self.z * other.x - self.x * other.z,
             self.x * other.y - self.y * other.x,
@@ -341,13 +340,13 @@ class Matrix: # pylint: disable = too-many-public-methods
     def translate(self, x, y, z):
         # type: (float, float, float) -> Matrix
         """
-        >>> identity().translate(5, -3, 2) @ Point(-3, 4, 5) == Point(2, 1, 7)
+        >>> identity().translate(5, -3, 2) @ Point3D(-3, 4, 5) == Point3D(2, 1, 7)
         True
 
-        >>> identity().translate(5, -3, 2).inverse() @ Point(-3, 4, 5) == Point(-8, 7, 3)
+        >>> identity().translate(5, -3, 2).inverse() @ Point3D(-3, 4, 5) == Point3D(-8, 7, 3)
         True
 
-        >>> identity().translate(5, -3, 2) @ Vector(-3, 4, 5) == Vector(-3, 4, 5)
+        >>> identity().translate(5, -3, 2) @ Vector3D(-3, 4, 5) == Vector3D(-3, 4, 5)
         True
         """
         return (
@@ -358,13 +357,13 @@ class Matrix: # pylint: disable = too-many-public-methods
     def scale(self, x, y, z):
         # type: (float, float, float) -> Matrix
         """
-        >>> identity().scale(2, 3, 4) @ Point(-4, 6, 8) == Point(-8, 18, 32)
+        >>> identity().scale(2, 3, 4) @ Point3D(-4, 6, 8) == Point3D(-8, 18, 32)
         True
 
-        >>> identity().scale(2, 3, 4) @ Vector(-4, 6, 8) == Vector(-8, 18, 32)
+        >>> identity().scale(2, 3, 4) @ Vector3D(-4, 6, 8) == Vector3D(-8, 18, 32)
         True
 
-        >>> identity().scale(2, 3, 4).inverse() @ Point(-4, 6, 8) == Point(-2, 2, 2)
+        >>> identity().scale(2, 3, 4).inverse() @ Point3D(-4, 6, 8) == Point3D(-2, 2, 2)
         True
         """
         return (
@@ -396,17 +395,17 @@ class Matrix: # pylint: disable = too-many-public-methods
     def shear(self, x_y, x_z, y_x, y_z, z_x, z_y):
         # type: (float, float, float, float, float, float) -> Matrix
         """
-        >>> identity().shear(1, 0, 0, 0, 0, 0) @ Point(2, 3, 4) == Point(5, 3, 4)
+        >>> identity().shear(1, 0, 0, 0, 0, 0) @ Point3D(2, 3, 4) == Point3D(5, 3, 4)
         True
-        >>> identity().shear(0, 1, 0, 0, 0, 0) @ Point(2, 3, 4) == Point(6, 3, 4)
+        >>> identity().shear(0, 1, 0, 0, 0, 0) @ Point3D(2, 3, 4) == Point3D(6, 3, 4)
         True
-        >>> identity().shear(0, 0, 1, 0, 0, 0) @ Point(2, 3, 4) == Point(2, 5, 4)
+        >>> identity().shear(0, 0, 1, 0, 0, 0) @ Point3D(2, 3, 4) == Point3D(2, 5, 4)
         True
-        >>> identity().shear(0, 0, 0, 1, 0, 0) @ Point(2, 3, 4) == Point(2, 7, 4)
+        >>> identity().shear(0, 0, 0, 1, 0, 0) @ Point3D(2, 3, 4) == Point3D(2, 7, 4)
         True
-        >>> identity().shear(0, 0, 0, 0, 1, 0) @ Point(2, 3, 4) == Point(2, 3, 6)
+        >>> identity().shear(0, 0, 0, 0, 1, 0) @ Point3D(2, 3, 4) == Point3D(2, 3, 6)
         True
-        >>> identity().shear(0, 0, 0, 0, 0, 1) @ Point(2, 3, 4) == Point(2, 3, 7)
+        >>> identity().shear(0, 0, 0, 0, 0, 1) @ Point3D(2, 3, 4) == Point3D(2, 3, 7)
         True
         """
         return (
@@ -415,17 +414,12 @@ class Matrix: # pylint: disable = too-many-public-methods
         )
 
 
-def Tuple4(x, y, z, w): # pylint: disable = invalid-name
-    # type: (float, float, float, float) -> Matrix
-    return Matrix([[x, y, z, w]])
-
-
-def Vector(x, y, z): # pylint: disable = invalid-name
+def Vector3D(x=0, y=0, z=0): # pylint: disable = invalid-name
     # type: (float, float, float) -> Matrix
     return Matrix([[x, y, z, 0]])
 
 
-def Point(x, y, z): # pylint: disable = invalid-name
+def Point3D(x=0, y=0, z=0): # pylint: disable = invalid-name
     # type: (float, float, float) -> Matrix
     return Matrix([[x, y, z, 1]])
 
