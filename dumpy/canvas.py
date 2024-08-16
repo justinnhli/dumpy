71a6275a8483c24638cba234d644ffa3c6a79c22
Justin Li <justinnhli@gmail.com>
2024-08-16 11:33:35 -0700

    [canvas] add Canvas and tests

---
+++ dumpy/canvas.py
@@ -0,0 +1,144 @@
+"""A canvas built in tk and Pillow."""
+
+from tkinter import CENTER, Tk, Canvas as TKCanvas, Event, NW
+from typing import Callable, Sequence
+
+from PIL import Image
+from PIL.ImageDraw import Draw
+from PIL.ImageTk import PhotoImage
+
+from .matrix import Point2D, Matrix
+from .color import Color
+
+
+class Canvas:
+
+    def __init__(self, size, title='', background=None):
+        # type: (Point2D, str, Color) -> None
+        """Initialize a Canvas."""
+        self.size = size
+        self.title = title
+        if background is None:
+            background = Color(0, 0, 1)
+        self.image = Image.new('RGBA', size=(self.size.x, self.size.y), color=background.to_rgba_tuple())
+        self.draw = Draw(self.image, 'RGBA')
+        # gets around weird casing in title
+        # see https://bugs.python.org/issue13553
+        self.tk = Tk(className=('\u200B' + self.title)) # pylint: disable = superfluous-parens
+        self.tk.minsize(self.size.x, self.size.y)
+
+        self.canvas = TKCanvas(
+            self.tk,
+            width=self.size.x, height=self.size.y,
+            background='#FFFFFF',
+        )
+        self.canvas.place(relx=.5, rely=.5, anchor=CENTER)
+        self.image_tk = PhotoImage(self.image)
+
+    # drawing functions
+
+    @staticmethod
+    def _set_default_colors(fill_color, line_color):
+        # type: (Color, Color) -> tuple[Color, Color]
+        if line_color is None:
+            if fill_color is None:
+                line_color = Color(0, 0, 0, 1)
+                fill_color = Color(0, 0, 0, 0)
+            else:
+                line_color = fill_color
+        elif fill_color is None:
+            fill_color = Color(0, 0, 0, 0)
+        return fill_color, line_color
+
+    def draw_pixel(self, point, color=None):
+        # type: (Matrix, Color) -> None
+        """Draw a pixel."""
+        if color is None:
+            color = Color(0, 0, 0)
+        self.image.putpixel((point.x, point.y), color.to_rgba_tuple())
+
+    def draw_poly(self, points, fill_color=None, line_color=None):
+        # type: (Sequence[Matrix], Color, Color) -> None
+        """Draw a polygon."""
+        fill_color, line_color = Canvas._set_default_colors(fill_color, line_color)
+        self.draw.polygon(
+            [(point.x, point.y) for point in points],
+            outline=line_color.to_rgba_tuple(),
+            fill=fill_color.to_rgba_tuple(),
+            width=1,
+        )
+
+    def draw_oval(self, point1, point2, fill_color=None, line_color=None):
+        # type: (Matrix, Matrix, Color, Color) -> None
+        """Draw an oval."""
+        fill_color, line_color = Canvas._set_default_colors(fill_color, line_color)
+        self.draw.ellipse(
+            [(point1.x, point1.y), (point2.x, point2.y)],
+            outline=line_color.to_rgba_tuple(),
+            fill=fill_color.to_rgba_tuple(),
+            width=1,
+        )
+
+    def draw_text(self, point, text, anchor=None):
+        # type: (Matrix, Color, Color) -> None
+        """Draw text."""
+        pass # FIXME
+
+    def update_page(self):
+        # type: () -> None
+        """Draw the page to the canvas."""
+        self.image_tk = PhotoImage(self.image)
+        self.canvas.create_image(
+            1, 1,
+            anchor=NW,
+            image=self.image_tk,
+        )
+
+    def _create_update_callback(self, update_fn, msecs):
+        # type: (Callable[[], None], float) -> Callable[[], None]
+        """Create the wrapped update callback function."""
+
+        def callback():
+            # type: () -> None
+            update_fn()
+            self.update_page()
+            self.tk.after(msecs, callback)
+
+        return callback
+
+    def display(self, update=None, secs=0):
+        # type: (Callable[[], None], float) -> None
+        """Display the canvas."""
+        self.canvas.focus_set()
+        if update is None:
+            self.update_page()
+        else:
+            if secs > 0:
+                msecs = int(1000 * secs)
+                self.tk.after(msecs, self._create_update_callback(update, msecs))
+            else:
+                self.tk.after(0, update)
+        self.canvas.mainloop()
+
+    # interaction functions
+
+    def bind_key(self, key, callback):
+        # type: (str, Callable[[Event[TKCanvas]], None]) -> None
+        """Add a keybind."""
+        # FIXME need parameters for modifier keys
+        self.canvas.bind(key, callback)
+
+    def bind_mouse_click(self, button, callback):
+        # type: (str, Callable[[Event[TKCanvas]], None]) -> None
+        """Bind a mouse click."""
+        if button == 'left':
+            self.canvas.bind('<Button-1>', callback)
+        elif button == 'middle':
+            raise NotImplementedError()
+        elif button == 'right':
+            self.canvas.bind('<Button-2>', callback)
+
+    def bind_mouse_movement(self, callback):
+        # type: (Callable[[Event[TKCanvas]], None]) -> None
+        """Bind mouse movement."""
+        self.canvas.bind('<Motion>', callback)
---
+++ tests/canvas_test.py
@@ -0,0 +1,107 @@
+"""Tests for canvas.py."""
+
+from itertools import batched
+from pathlib import Path
+from typing import Iterator
+
+from PIL import Image, ImageGrab
+
+from dumpy.canvas import Canvas
+from dumpy.matrix import Point2D
+from dumpy.color import Color
+
+
+def ppm_value_generator(path):
+    # type: (Path) -> Iterator[str]
+    """Read from a ppm file one value at a time."""
+    with path.open() as fd:
+        for line in fd:
+            yield from line.strip().split()
+
+
+def read_ppm(path):
+    # type: (Path) -> tuple[int, int, list[tuple[int, int, int]]]
+    """Read an image from a ppm file."""
+    # create the generator
+    generator = ppm_value_generator(path)
+    # read the magic number
+    assert next(generator) == 'P3'
+    # read the width and height
+    width, height = int(next(generator)), int(next(generator))
+    # read (and discard) the bit depth
+    next(generator)
+    # create the pixel map
+    pixels = [
+        (int(r), int(g), int(b))
+        for r, g, b in batched(generator, n=3)
+    ]
+    return width, height, pixels
+
+
+def grab_screen(canvas):
+    # type: (Canvas) -> Image
+    """Take a screenshot of a canvas."""
+    x0 = canvas.tk.winfo_rootx() + canvas.canvas.winfo_x() + 1
+    y0 = canvas.tk.winfo_rooty() + canvas.canvas.winfo_y() + 1
+    x1 = x0 + canvas.canvas.winfo_width() - 2
+    y1 = y0 + canvas.canvas.winfo_height() - 2
+    return ImageGrab.grab().crop((x0, y0, x1, y1))
+
+
+def assert_canvas(canvas, filename, write=False):
+    # type: (Canvas, str, bool) -> None
+    """Check that a canvas matches the file."""
+    canvas.update_page()
+    canvas.canvas.update()
+    image = grab_screen(canvas)
+    canvas.tk.destroy()
+    pixels = image.getdata()
+    ppm_path = Path(__file__).parent / 'canvas_test_images' / filename
+    width, height, ppm_pixels = read_ppm(ppm_path)
+    assert image.width == width
+    assert image.height == height
+    pixels = image.getdata()
+    for x in range(width):
+        for y in range(height):
+            index = x * width + y
+            assert pixels[index] == ppm_pixels[index]
+
+
+def test_canvas_pixel():
+    # type: () -> None
+    """Test drawing a pixel."""
+    canvas = Canvas(Point2D(3, 3), 'test')
+    canvas.draw_pixel(Point2D(1, 1))
+    assert_canvas(canvas, 'canvas_pixel_test.ppm')
+
+
+def test_canvas_rect():
+    # type: () -> None
+    """Test drawing a filled rectangle."""
+    canvas = Canvas(Point2D(5, 5), 'test')
+    canvas.draw_poly(
+        [
+            Point2D(1, 1),
+            Point2D(1, 3),
+            Point2D(3, 3),
+            Point2D(3, 1),
+        ],
+        fill_color=Color.from_hex('#000000'),
+    )
+    assert_canvas(canvas, 'canvas_rect_test.ppm')
+
+
+def test_canvas_rect_outline():
+    # type: () -> None
+    """Test drawing a rectangle outline."""
+    canvas = Canvas(Point2D(5, 5), 'test')
+    canvas.draw_poly(
+        [
+            Point2D(1, 1),
+            Point2D(1, 3),
+            Point2D(3, 3),
+            Point2D(3, 1),
+        ],
+        line_color=Color.from_hex('#000000'),
+    )
+    assert_canvas(canvas, 'canvas_rect_outline_test.ppm')
