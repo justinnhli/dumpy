1f9b13d3b4fb68f8fa55c6a75d7af573ebeebedf
Justin Li <justinnhli@gmail.com>
2024-11-21 10:12:52 -0800

    [geometry2d] add geometry2d.py

---
+++ dumpy/geometry2d.py
@@ -0,0 +1,139 @@
+"""2D geometry objects."""
+
+from typing import Optional
+
+from .matrix import Matrix, Point2D
+
+
+class Segment:
+    """A line segment."""
+
+    def __init__(self, point1, point2):
+        # type: (Matrix, Matrix) -> None
+        if (point1.x, point1.y) < (point2.x, point2.y):
+            point1, point2 = point2, point1
+        self.point1 = point1
+        self.point2 = point2
+
+    @property
+    def min_x(self):
+        # type: () -> float
+        """The smaller x-coordinate."""
+        return min(self.point1.x, self.point2.x)
+
+    @property
+    def max_x(self):
+        # type: () -> float
+        """The larger x-coordinate."""
+        return max(self.point1.x, self.point2.x)
+
+    @property
+    def min_y(self):
+        # type: () -> float
+        """The smaller y-coordinate."""
+        return min(self.point1.y, self.point2.y)
+
+    @property
+    def max_y(self):
+        # type: () -> float
+        """The larger y-coordinate."""
+        return max(self.point1.y, self.point2.y)
+
+    @property
+    def slope(self):
+        # type: () -> float
+        """The slope of the segment."""
+        denominator = self.point2.x - self.point1.x
+        if denominator == 0:
+            return float('Inf')
+        else:
+            return (self.point2.y - self.point1.y) / denominator
+
+    def __str__(self):
+        # type: () -> str
+        return f'Segment({self.point1}, {self.point2})'
+
+    def is_parallel(self, other):
+        # type: (Segment) -> bool
+        """Return whether the other segment is parallel."""
+        return self.slope == other.slope
+
+    @staticmethod
+    def _orientation(p1, p2, p3):
+        # type: (Point2D, Point2D, Point2D) -> int
+        """Determine the orientation going from p1 to p2 to p3."""
+        val = (
+            ((p2.y - p1.y) * (p3.x - p2.x))
+            - ((p2.x - p1.x) * (p3.y - p2.y))
+        )
+        if val < 0: # counterclockwise
+            return -1
+        elif val > 0: # clockwise
+            return 1
+        else: # co-linear
+            return 0
+
+    def contains(self, point, include_end=True):
+        # type: (Point2D, bool) -> bool
+        """Return True if the point is on the segment."""
+        if include_end:
+            return (
+                (point.x <= max(self.point1.x, self.point2.x))
+                and (point.x >= min(self.point1.x, self.point2.x))
+                and (point.y <= max(self.point1.y, self.point2.y))
+                and (point.y >= min(self.point1.y, self.point2.y))
+            )
+        else:
+            return (
+                (point.x < max(self.point1.x, self.point2.x))
+                and (point.x > min(self.point1.x, self.point2.x))
+                and (point.y < max(self.point1.y, self.point2.y))
+                and (point.y > min(self.point1.y, self.point2.y))
+            )
+
+    def intersect(self, other, include_end=True):
+        # type: (Segment, bool) -> Optional[Point2D]
+        """Find the intersection with another segment, if any."""
+        bounding_box_overlaps = (
+            (
+                self.min_x <= other.min_x <= self.max_x
+                or other.min_x <= self.min_x <= other.max_x
+            ) and (
+                self.min_y <= other.min_y <= self.max_y
+                or other.min_y <= self.min_y <= other.max_y
+            )
+        )
+        if not bounding_box_overlaps:
+            return None
+        o1 = Segment._orientation(self.point1, other.point1, other.point2)
+        o2 = Segment._orientation(self.point2, other.point1, other.point2)
+        o3 = Segment._orientation(other.point1, self.point1, self.point2)
+        o4 = Segment._orientation(other.point2, self.point1, self.point2)
+        # general case: no co-linearity
+        if 0 not in (o1, o2, o3, o4):
+            vector1 = self.point2 - self.point1
+            vector2 = other.point2 - other.point1
+            perpendicular1 = Point2D(-vector2.y, vector2.x)
+            proportion1 = (other.point1 - self.point1).dot(perpendicular1) / vector1.dot(perpendicular1)
+            perpendicular2 = Point2D(-vector1.y, vector1.x)
+            proportion2 = (self.point1 - other.point1).dot(perpendicular2) / vector2.dot(perpendicular2)
+            if 0 <= proportion1 <= 1 and 0 <= proportion2 <= 1:
+                if include_end or (proportion1 not in (0, 1) and proportion2 not in (0, 1)):
+                    return self.point1 + vector1 * proportion1
+            return None
+        if not include_end:
+            return None
+        if o1 == 0 and other.contains(self.point1, include_end=True):
+            # p1, q1 and p2 are collinear and p2 lies on segment p1q1
+            return self.point1
+        elif o2 == 0 and other.contains(self.point2, include_end=True):
+            # p1, q1 and q2 are collinear and q2 lies on segment p1q1
+            return self.point2
+        elif o3 == 0 and self.contains(other.point1, include_end=True):
+            # p2, q2 and p1 are collinear and p1 lies on segment p2q2
+            return other.point1
+        elif o4 == 0 and self.contains(other.point2, include_end=True):
+            # p2, q2 and q1 are collinear and q1 lies on segment p2q2
+            return other.point2
+        else:
+            return None
---
+++ tests/geometry2d_test.py
@@ -0,0 +1,43 @@
+"""Tests for geometry2d.py."""
+
+from dumpy.matrix import Point2D
+from dumpy.geometry2d import Segment
+
+
+def test_segment():
+    # at most one point of intersection, include_end=True
+    segments = [
+        # vertical second segment
+        (Segment(Point2D(0, 2), Point2D(0, 4)), (Point2D(0, 2), None)), # parallel, co-linear, end-end
+        (Segment(Point2D(1, 2), Point2D(1, 4)), (None, None)), # parallel, not co-linear, not intersecting
+        (Segment(Point2D(1, 1), Point2D(1, 3)), (None, None)), # parallel, not co-linear, not intersecting
+        (Segment(Point2D(1, 0), Point2D(1, 2)), (None, None)), # parallel, not co-linear, not intersecting
+        # horizontal second segment
+        (Segment(Point2D(-1, 3), Point2D(1, 3)), (None, None)), # not parallel, not co-linear, not intersecting
+        (Segment(Point2D(-1, 2), Point2D(1, 2)), (Point2D(0, 2), None)), # not parallel, co-linear, middle-end
+        (Segment(Point2D(-1, 1), Point2D(1, 1)), (Point2D(0, 1), Point2D(0, 1))), # not parallel, not co-linear, middle-middle
+        (Segment(Point2D(0, 3), Point2D(2, 3)), (None, None)), # not parallel, not co-linear, not intersecting
+        (Segment(Point2D(0, 2), Point2D(2, 2)), (Point2D(0, 2), None)), # not parallel, co-linear, end-end
+        (Segment(Point2D(0, 1), Point2D(2, 1)), (Point2D(0, 1), None)), # not parallel, co-linear, middle-end
+    ]
+    segment1 = Segment(Point2D(0, 0), Point2D(0, 2))
+    for segment2, expecteds in segments:
+        for expected, include_end in zip(expecteds, (True, False)):
+            answer12 = segment1.intersect(segment2, include_end=include_end)
+            answer21 = segment2.intersect(segment1, include_end=include_end)
+            if expected is None:
+                assert answer12 is None and answer21 is None
+            else:
+                assert answer12 == answer21 == expected
+    # more than one point of intersection
+    segment2 = Segment(Point2D(0, 1), Point2D(0, 3))
+    for answer in [segment1.intersect(segment2), segment2.intersect(segment1)]:
+        assert answer.x == 0 and 1 <= answer.y <= 2
+    segment2 = Segment(Point2D(0, 0), Point2D(0, 2))
+    for answer in [segment1.intersect(segment2), segment2.intersect(segment1)]:
+        assert answer.x == 0 and 0 <= answer.y <= 2
+    # bounding box intersects but not segments
+    segment1 = Segment(Point2D(0, 4), Point2D(4, 0))
+    segment2 = Segment(Point2D(0, 0), Point2D(1, 1))
+    assert segment1.intersect(segment2) is None
+    assert segment2.intersect(segment1) is None
