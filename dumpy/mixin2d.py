f3362893a4c76f63135a8baa76efc8afa77024ac
Justin Li <justinnhli@gmail.com>
2024-12-08 19:51:53 -0800

    [mixin2d] add Transform2DMixIn

---
+++ dumpy/mixin2d.py
@@ -0,0 +1,18 @@
+"""Mix-in classes for 2D."""
+
+from typing import Any
+
+from .matrix import Matrix, Point2D
+
+
+class Transform2DMixIn:
+    """MixIn for transform information."""
+
+    def __init__(self, *args, position=None, rotation=0, **kwargs):
+        # type: (Any, Matrix, float, Any) -> None
+        """Initialize position and rotation."""
+        # pylint: disable = unused-argument
+        if position is None:
+            position = Point2D(0, 0)
+        self.position = position
+        self.rotation = rotation
