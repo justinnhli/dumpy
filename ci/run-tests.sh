60fe162ba0b022964e01ec7e6c07054e854af230
Justin Li <justinnhli@gmail.com>
2024-12-27 17:40:13 -0800

    [meta] merge testing and main requirements

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # set python3 executable
-PYTHON3=$HOME/.local/share/venv/dumpy-test/bin/python3
+PYTHON3=$HOME/.local/share/venv/dumpy/bin/python3
 # run tests
 $PYTHON3 -m coverage run -m pytest "$@"
 # generate coverage report if all tests were run
