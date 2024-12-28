bdec410f09492cc0f5eeb0a4e4810b3f536f8bce
Justin Li <justinnhli@gmail.com>
2024-12-27 18:14:22 -0800

    [mixins] introduce Transform class

--- dumpy/mixins.py
+++ dumpy/mixins.py
@@ -1,18 +1,67 @@
 """Mix-in classes."""
 
+from functools import cached_property
 from typing import Any
 
-from .matrix import Matrix, Point2D
+from .matrix import Matrix, Point2D, identity
+
+
+class Transform:
+    """A transform."""
+
+    def __init__(self, translation=None, rotation=0):
+        # type: (Matrix, float) -> None
+        """Initialize the Transform."""
+        if translation is None:
+            translation = Point2D()
+        self.translation = translation
+        self.rotation = rotation
+
+    @cached_property
+    def x(self):
+        # type: () -> float
+        """Return the x value of the translation."""
+        return self.translation.x
+
+    @cached_property
+    def y(self):
+        # type: () -> float
+        """Return the y value of the translation."""
+        return self.translation.y
+
+    @cached_property
+    def theta(self):
+        # type: () -> float
+        """Return the rotation."""
+        return self.rotation
+
+    @cached_property
+    def matrix(self):
+        # type: () -> Matrix
+        """Create the transformation matrix."""
+        return (
+            identity(3)
+            .rotate_z(self.rotation)
+            .translate(self.translation.x, self.translation.y, 0)
+        )
+
+    def __add__(self, other):
+        # type: (Transform) -> Transform
+        return Transform(
+            Point2D(self.x + other.x, self.y + other.y),
+            self.rotation + other.rotation,
+        )
+
+    def __neg__(self):
+        # type: () -> Transform
+        return Transform(-self.translation, -self.rotation)
 
 
 class TransformMixIn:
-    """MixIn for transform information."""
+    """MixIn for objects with translation and rotation."""
 
-    def __init__(self, *args, position=None, rotation=0, **kwargs):
+    def __init__(self, *args, translation=None, rotation=0, **kwargs):
         # type: (Any, Matrix, float, Any) -> None
-        """Initialize position and rotation."""
+        """Initialize the Transform."""
         # pylint: disable = unused-argument
-        if position is None:
-            position = Point2D(0, 0)
-        self.position = position
-        self.rotation = rotation
+        self.transform = Transform(translation, rotation)
