add46e38b2e8b6bc5aeb38475ee31a001f2c3bf8
Justin Li <justinnhli@gmail.com>
2025-02-15 23:18:47 -0800

    [basic-window] fix BasicWindow

--- dumpy/basic_window.py
+++ dumpy/basic_window.py
@@ -1,12 +1,10 @@
 """A basic window for basic drawing."""
 
-from math import pi as PI
 from tkinter import Event
 
 from .camera import Camera
 from .canvas import Canvas
-from .matrix import Point2D, Vector2D
-from .mixins import Transform, TransformMixIn
+from .simplex import PointsMatrix
 
 
 class BasicWindow:
@@ -14,23 +12,23 @@ class BasicWindow:
 
     def __init__(self, width, height):
         # type: (int, int) -> None
-        self.canvas = Canvas(Point2D(width, height))
+        self.canvas = Canvas(width, height)
         self.camera = Camera(self.canvas)
         for key in ['w', 's', 'a', 'd', 'e', 'q', 'space', 'r', 'f']:
             self.canvas.bind_key(f'<{key}>', self.key_callback)
-        self.geometries = [] # type: list[TransformMixIn]
+        self.points_matrixes = [] # type: list[PointsMatrix]
 
     def draw(self, geometry):
-        # type: (TransformMixIn) -> None
+        # type: (PointsMatrix) -> None
         """Add a geometry to be drawn later."""
-        self.geometries.append(geometry)
+        self.points_matrixes.append(geometry)
 
     def _draw(self):
         # type: () -> None
         """Draw all geometries."""
         self.canvas.new_page()
-        for geometry in self.geometries:
-            geometry.draw(self.camera)
+        for points_matrix in self.points_matrixes:
+            self.camera.draw_points_matrix(points_matrix)
 
     def start(self):
         # type: () -> None
@@ -42,19 +40,19 @@ class BasicWindow:
         """Deal with key presses."""
         translation = 25 / self.camera.zoom
         if event.keysym == 'w':
-            self.camera.move(Transform(Vector2D(0, translation)))
+            self.camera.move_by(0, translation)
         elif event.keysym == 's':
-            self.camera.move(Transform(Vector2D(0, -translation)))
+            self.camera.move_by(0, -translation)
         elif event.keysym == 'a':
-            self.camera.move(Transform(Vector2D(-translation, 0)))
+            self.camera.move_by(-translation, 0)
         elif event.keysym == 'd':
-            self.camera.move(Transform(Vector2D(translation, 0)))
+            self.camera.move_by(translation, 0)
         elif event.keysym == 'q':
-            self.camera.move(Transform(rotation=0.125))
+            self.camera.rotate_by(0.125)
         elif event.keysym == 'e':
-            self.camera.move(Transform(rotation=-0.125))
+            self.camera.rotate_by(-0.125)
         elif event.keysym == 'space':
-            self.camera.move_to(Transform())
+            self.camera.move_to(0, 0)
         elif event.keysym == 'r':
             self.camera.zoom_level -= 1
         elif event.keysym == 'f':
