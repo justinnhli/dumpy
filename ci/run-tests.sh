223b4d2f65e6aaad5211a79f6476e1792610e012
Justin Li <justinnhli@gmail.com>
2025-02-17 10:20:20 -0800

    [meta] give verbose test output and always generate coverage report

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 # set python3 executable
 PYTHON3=$HOME/.local/share/venv/dumpy/bin/python3
 # run tests
-$PYTHON3 -m coverage run -m pytest "$@"
+$PYTHON3 -m coverage run -m pytest -v "$@"
 # generate coverage report if all tests were run
-if [ -z "$@" ]; then
-    $PYTHON3 -m coverage html
-fi
+$PYTHON3 -m coverage html
