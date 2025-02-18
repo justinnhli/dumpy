e17e695c9332d103258a284f09842f6f1570dab3
Justin Li <justinnhli@gmail.com>
2025-02-17 22:23:49 -0800

    [polygon] expand Polygon tests

--- tests/polygon_test.py
+++ tests/polygon_test.py
@@ -1,11 +1,25 @@
 """Tests for polygon.py."""
 
 from dumpy.polygon import Polygon
-from dumpy.simplex import Point2D
+from dumpy.simplex import Point2D, Segment
 
 
 def test_polygon():
-    polygon = Polygon.rectangle(2, 2)
-    assert polygon.area == 4
-    assert polygon.centroid == Point2D()
-    assert Polygon(*polygon.init_args) == polygon
+    rectangle = Polygon.rectangle(2, 2)
+    assert rectangle.area == 4
+    assert rectangle.centroid == Point2D()
+    assert Polygon(*rectangle.init_args) == rectangle
+    ellipse = round(Polygon.ellipse(20, 10, num_points=4), 3)
+    point1 = Point2D(-20, 0)
+    point2 = Point2D(-0, -10)
+    point3 = Point2D(20, 0)
+    point4 = Point2D(0, 10)
+    assert ellipse.points == (
+        point1, point2, point3, point4
+    )
+    assert ellipse.segments == (
+        Segment(point1, point2),
+        Segment(point2, point3),
+        Segment(point3, point4),
+        Segment(point4, point1),
+    )
