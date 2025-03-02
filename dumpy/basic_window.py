6e111ac1baf7dc44a41904a250c753d295150511
Justin Li <justinnhli@gmail.com>
2025-03-02 15:50:12 -0800

    [basic-window] inherit from Game

--- dumpy/basic_window.py
+++ dumpy/basic_window.py
@@ -1,68 +1,63 @@
 """A basic window for basic drawing."""
 
 from math import sin, cos
-from tkinter import Event
 
-from .camera import Camera
-from .canvas import Canvas
 from .color import Color
 from .simplex import PointsMatrix
+from .game import Game
+from .game_object import GameObject
 
 
-class BasicWindow:
+class DummyGameObject(GameObject):
+    """A dummy game object to hold a static geometry."""
+
+    def __init__(self, polygon, fill_color=None, line_color=None):
+        # type: (PointsMatrix, Color, Color) -> None
+        super().__init__()
+        self.points_matrix = polygon
+        self.fill_color = fill_color
+        self.line_color = line_color
+
+
+class BasicWindow(Game):
     """A basic window for drawing static geometries."""
 
     def __init__(self, width, height):
         # type: (int, int) -> None
-        self.canvas = Canvas(width, height)
-        self.camera = Camera(self.canvas)
-        for key in ['w', 's', 'a', 'd', 'e', 'q', 'space', 'r', 'f']:
-            self.canvas.bind_key(f'<{key}>', self.key_callback)
-        self.points_matrixes = [] # type: list[tuple[PointsMatrix, Color, Color, Color]]
+        super().__init__(width, height)
+        for keysym in ['w', 's', 'a', 'd', 'e', 'q', 'space', 'r', 'f']:
+            self.bind_key(f'<{keysym}>', self.key_callback)
 
-    def draw(self, points_matrix, color=None, fill_color=None, line_color=None):
-        # type: (PointsMatrix, Color, Color, Color) -> None
-        """Add a PointsMatrix to be drawn later."""
-        self.points_matrixes.append((points_matrix, color, fill_color, line_color))
+    def add_geometry(self, polygon, fill_color=None, line_color=None):
+        # type: (PointsMatrix, Color, Color) -> None
+        """Add a geometry to be drawn."""
+        self.add_object(DummyGameObject(
+            polygon,
+            fill_color=fill_color,
+            line_color=line_color,
+        ))
 
-    def _draw(self):
-        # type: () -> None
-        """Draw all geometries."""
-        self.canvas.new_page()
-        for points_matrix, color, fill_color, line_color in self.points_matrixes:
-            self.camera.draw_points_matrix(
-                points_matrix,
-                color=color,
-                fill_color=fill_color,
-                line_color=line_color,
-            )
-
-    def start(self):
-        # type: () -> None
-        """Display the canvas."""
-        self.canvas.start(self._draw, 1 / 25)
-
-    def key_callback(self, event):
-        # type: (Event) -> None
+    def key_callback(self, keysym):
+        # type: (str) -> None
         """Deal with key presses."""
         translation = 25 / self.camera.zoom
-        if event.keysym == 'w':
+        if keysym == 'w':
             self.camera.move_by(translation * sin(-self.camera.radians), translation * cos(-self.camera.radians))
-        elif event.keysym == 's':
+        elif keysym == 's':
             self.camera.move_by(-translation * sin(-self.camera.radians), -translation * cos(-self.camera.radians))
-        elif event.keysym == 'a':
+        elif keysym == 'a':
             self.camera.move_by(-translation * cos(self.camera.radians), -translation * sin(self.camera.radians))
-        elif event.keysym == 'd':
+        elif keysym == 'd':
             self.camera.move_by(translation * cos(self.camera.radians), translation * sin(self.camera.radians))
-        elif event.keysym == 'q':
+        elif keysym == 'q':
             self.camera.rotate_by(0.125)
-        elif event.keysym == 'e':
+        elif keysym == 'e':
             self.camera.rotate_by(-0.125)
-        elif event.keysym == 'space':
+        elif keysym == 'space':
             self.camera.move_to(0, 0)
             self.camera.rotate_to(0)
             self.camera.zoom_level = 0
-        elif event.keysym == 'r':
+        elif keysym == 'r':
             self.camera.zoom_level += 1
-        elif event.keysym == 'f':
+        elif keysym == 'f':
             self.camera.zoom_level -= 1
