30ee793a51d463027dda8b6acf5cbde1a95e7fd0
Justin Li <justinnhli@gmail.com>
2025-03-02 15:58:01 -0800

    [scene] add placeholder Scene class

---
+++ dumpy/scene.py
@@ -0,0 +1,22 @@
+"""A scene of objects."""
+
+from .camera import Camera
+from .game_object import GameObject
+
+
+class Scene:
+
+    def __init__(self):
+        # type: () -> None
+        """Initialize the Scene."""
+        self.objects = []
+
+    def add(self, game_object):
+        # type: (GameObject) -> None
+        """Add an object to the scene."""
+        self.objects.append(game_object)
+
+    def get_in_view(self, camera):
+        # type: (Camera) -> list[GameObject]
+        """Get all objects in view of the camera."""
+        return self.objects # FIXME
