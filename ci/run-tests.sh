b8ae0f7bfae4b23a7961f19edb7b97819cf54289
Justin Li <justinnhli@gmail.com>
2025-03-02 17:55:17 -0800

    [meta] only show coverage for dumpy source files

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 # set python3 executable
 PYTHON3=$HOME/.local/share/venv/dumpy/bin/python3
 # run tests
-$PYTHON3 -m coverage run -m pytest -v "$@"
+$PYTHON3 -m coverage run --source=dumpy -m pytest -v "$@"
 # generate coverage report if all tests were run
 $PYTHON3 -m coverage html
