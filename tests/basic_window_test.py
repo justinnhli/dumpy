a6a323f01fe740a5f0bc756da6b63ef0aeefa00a
Justin Li <justinnhli@gmail.com>
2025-03-23 10:17:16 -0700

    [basic-window] add key press tests

--- tests/basic_window_test.py
+++ tests/basic_window_test.py
@@ -3,6 +3,7 @@
 from math import sqrt
 
 from dumpy.basic_window import BasicWindow
+from dumpy.canvas import Input
 from dumpy.color import Color
 from dumpy.polygon import Polygon
 from dumpy.simplex import Point2D
@@ -69,3 +70,123 @@ def test_einstein():
     window.camera.move_to(einstein.centroid.x, einstein.centroid.y)
     window.dispatch_tick()
     check_image(window.canvas.image, 'basic_window__einstein.ppm')
+
+
+def test_up():
+    # type: () -> None
+    """Move the camera up."""
+    *_, einstein = create_einstein()
+    window = BasicWindow(600, 400)
+    window.add_geometry(einstein)
+    for _ in range(3):
+        window.key_callback(
+            Input('KeyPress', key_button='w'),
+            Point2D(),
+        )
+    window.dispatch_tick()
+    check_image(window.canvas.image, 'basic_window__up.ppm')
+
+
+def test_down():
+    # type: () -> None
+    """Move the camera down."""
+    *_, einstein = create_einstein()
+    window = BasicWindow(600, 400)
+    window.add_geometry(einstein)
+    for _ in range(3):
+        window.key_callback(
+            Input('KeyPress', key_button='s'),
+            Point2D(),
+        )
+    window.dispatch_tick()
+    check_image(window.canvas.image, 'basic_window__down.ppm')
+
+
+def test_left():
+    # type: () -> None
+    """Move the camera left."""
+    *_, einstein = create_einstein()
+    window = BasicWindow(600, 400)
+    window.add_geometry(einstein)
+    for _ in range(3):
+        window.key_callback(
+            Input('KeyPress', key_button='a'),
+            Point2D(),
+        )
+    window.dispatch_tick()
+    check_image(window.canvas.image, 'basic_window__left.ppm')
+
+
+def test_right():
+    # type: () -> None
+    """Move the camera right."""
+    *_, einstein = create_einstein()
+    window = BasicWindow(600, 400)
+    window.add_geometry(einstein)
+    for _ in range(3):
+        window.key_callback(
+            Input('KeyPress', key_button='d'),
+            Point2D(),
+        )
+    window.dispatch_tick()
+    check_image(window.canvas.image, 'basic_window__right.ppm')
+
+
+def test_clockwise():
+    # type: () -> None
+    """Rotate the camera clockwise."""
+    *_, einstein = create_einstein()
+    window = BasicWindow(600, 400)
+    window.add_geometry(einstein)
+    for _ in range(3):
+        window.key_callback(
+            Input('KeyPress', key_button='e'),
+            Point2D(),
+        )
+    window.dispatch_tick()
+    check_image(window.canvas.image, 'basic_window__clockwise.ppm')
+
+
+def test_counter_clockwise():
+    # type: () -> None
+    """Rotate the camera counter-clockwise."""
+    *_, einstein = create_einstein()
+    window = BasicWindow(600, 400)
+    window.add_geometry(einstein)
+    for _ in range(3):
+        window.key_callback(
+            Input('KeyPress', key_button='q'),
+            Point2D(),
+        )
+    window.dispatch_tick()
+    check_image(window.canvas.image, 'basic_window__counter_clockwise.ppm')
+
+
+def test_zoom_in():
+    # type: () -> None
+    """Zoom the camera in."""
+    *_, einstein = create_einstein()
+    window = BasicWindow(600, 400)
+    window.add_geometry(einstein)
+    for _ in range(3):
+        window.key_callback(
+            Input('KeyPress', key_button='r'),
+            Point2D(),
+        )
+    window.dispatch_tick()
+    check_image(window.canvas.image, 'basic_window__in.ppm')
+
+
+def test_zoom_out():
+    # type: () -> None
+    """Zoom the camera out."""
+    *_, einstein = create_einstein()
+    window = BasicWindow(600, 400)
+    window.add_geometry(einstein)
+    for _ in range(3):
+        window.key_callback(
+            Input('KeyPress', key_button='f'),
+            Point2D(),
+        )
+    window.dispatch_tick()
+    check_image(window.canvas.image, 'basic_window__out.ppm')
