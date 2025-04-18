50f7119311e86bf25d43cee95ac7c9f6429decba
Justin Li <justinnhli@gmail.com>
2025-04-17 22:06:04 -0700

    [scene] move collision callbacks out

--- dumpy/scene.py
+++ dumpy/scene.py
@@ -1,14 +1,10 @@
 """A scene of objects."""
 
-from collections import defaultdict
 from itertools import product, combinations
-from typing import Callable
+from typing import Iterator, Iterable
 
 from .camera import Camera
 from .game_object import GameObject
-
-
-CollisionCallback = Callable[[], None] # FIXME
 
 
 class Scene:
@@ -18,38 +14,36 @@ class Scene:
         # type: () -> None
         """Initialize the Scene."""
         self.objects = [] # type: list[GameObject]
-        self.groups = defaultdict(set) # type: dict[int, set[str]]
-        self.collision_groups = {} # type: dict[frozenset[str], CollisionCallback]
+        self.collision_group_pairs = set() # type: set[tuple[str, str]]
 
-    def add(self, game_object, *groups):
-        # type: (GameObject, *str) -> None
+    @property
+    def collisions(self):
+        # type: () -> Iterator[tuple[GameObject, GameObject, tuple[str, str]]]
+        """Yield all collisions of registered group pairs."""
+        # FIXME horribly inefficient
+        for obj1, obj2 in combinations(self.objects, 2):
+            if not obj1.is_colliding(obj2):
+                continue
+            for group1, group2 in product(obj1.collision_groups, obj2.collision_groups):
+                key = (group1, group2)
+                if key in self.collision_group_pairs:
+                    yield obj1, obj2, key
+                key = (group2, group1)
+                if key in self.collision_group_pairs:
+                    yield obj2, obj1, key
+
+    def add(self, game_object):
+        # type: (GameObject) -> None
         """Add an object to the scene."""
         self.objects.append(game_object)
-        self.groups[id(game_object)] |= set(groups)
 
     def get_in_view(self, camera):
         # type: (Camera) -> list[GameObject]
         """Get all objects in view of the camera."""
         return self.objects # FIXME
 
-    def register_collision(self, group1, group2, callback):
-        # type: (str, str, CollisionCallback) -> None
-        """Register two object groups for collision detection."""
-        self.collision_groups[frozenset([group1, group2])] = callback
-
-    def trigger_collisions(self):
-        # type: () -> None
-        """Trigger object collision callbacks."""
-        # FIXME horribly inefficient
-        for obj1, obj2 in combinations(self.objects, 2):
-            if not self.colliding(obj1, obj2):
-                continue
-            for group1, group2 in product(self.groups[id(obj1)], self.groups[id(obj2)]):
-                key = frozenset([group1, group2])
-                if key in self.collision_groups:
-                    self.collision_groups[key]()
-
-    def colliding(self, obj1, obj2):
-        # type: (GameObject, GameObject) -> bool
-        """Determine if two objects are colliding."""
-        return False # FIXME
+    def set_collision_group_pairs(self, collision_pairs):
+        # type: (Iterable[tuple[str, str]]) -> None
+        """Set collision group pairs to detect."""
+        for group_pair in collision_pairs:
+            self.collision_group_pairs.add(group_pair)
