1e741ffd95f59c79f5a16e3f061302ea7cc114f1
Justin Li <justinnhli@gmail.com>
2025-05-10 15:12:34 -0700

    [metaprogramming] simplify test classes

--- tests/metaprogramming_test.py
+++ tests/metaprogramming_test.py
@@ -1,24 +1,33 @@
+"""Tests for metaprogramming.py."""
+# pylint: disable = comparison-with-callable, too-few-public-methods
+
 from dumpy.metaprogramming import cached_class, cached_property
 
 
 @cached_class
-class Dummy:
+class CachedClassDummy:
+    """A dummy class to test cached_class."""
 
     def __init__(self, x):
+        # type: (int) -> None
         self.x = x
         self.num_calls = 0
 
     @cached_property('x')
     def prop(self):
+        # type: () -> str
+        """Test property."""
         self.num_calls += 1
         return f'prop {self.x}'
 
 
 def test_cached_class():
-    x = Dummy(0)
-    y = Dummy(0)
-    z = Dummy(1)
-    w = Dummy(2)
+    # type: () -> None
+    """Test cached_class."""
+    x = CachedClassDummy(0)
+    y = CachedClassDummy(0)
+    z = CachedClassDummy(1)
+    w = CachedClassDummy(2)
     assert x is y
     assert x is not z
     assert x is not w
@@ -31,67 +40,62 @@ def test_cached_class():
     assert w.num_calls == 0
 
 
-class CPTest1:
+class CachedPropertyDummy:
+    """A dummy class to test cached_property."""
 
     def __init__(self, value):
         # type: (int) -> None
-        self.value = value
-        self.called = False
-
-    @cached_property('value')
-    def add_one(self):
-        # type: () -> int
-        self.called = True
-        return self.value + 1
-
-
-class CPTest2:
-
-    def __init__(self, value):
         self.value = value
         self.add_one_called = False
         self.add_two_called = False
 
     @cached_property('value')
     def add_one(self):
+        # type: () -> int
+        """Add one to value."""
         self.add_one_called = True
         return self.value + 1
 
     @cached_property('add_one')
     def add_two(self):
+        # type: () -> int
+        """Add two to value."""
         self.add_two_called = True
         return self.add_one + 1
 
 
 def test_cached_property_basic():
     # type: () -> None
-    obj1 = CPTest1(1)
-    assert not obj1.called
+    """Test cached_property."""
+    obj1 = CachedPropertyDummy(1)
+    assert not obj1.add_one_called
     assert obj1.add_one == 2, obj1.add_one
-    assert obj1.called
-    obj1.called = False
-    assert not obj1.called
+    assert obj1.add_one_called
+    obj1.add_one_called = False
+    assert not obj1.add_one_called
     assert obj1.add_one == 2
-    assert not obj1.called
+    assert not obj1.add_one_called
     obj1.add_one = 0
     assert obj1.add_one == 0
-    assert not obj1.called
+    assert not obj1.add_one_called
     obj1.value = 2
     assert obj1.value == 2
-    assert not obj1.called
+    assert not obj1.add_one_called
     assert obj1.add_one == 3
-    assert obj1.called
-    obj1.called = False
-    assert not obj1.called
+    assert obj1.add_one_called
+    obj1.add_one_called = False
+    assert not obj1.add_one_called
     assert obj1.add_one == 3
-    assert not obj1.called
-    obj2 = CPTest1(4)
+    assert not obj1.add_one_called
+    obj2 = CachedPropertyDummy(4)
     assert obj2.add_one == 5, obj2.add_one
     assert obj2.add_one == 5, obj2.add_one
 
 
 def test_cached_property_chained():
-    obj = CPTest2(10)
+    # type: () -> None
+    """Test cached_property chains."""
+    obj = CachedPropertyDummy(10)
     assert not obj.add_one_called
     assert not obj.add_two_called
     assert obj.add_one == 11
