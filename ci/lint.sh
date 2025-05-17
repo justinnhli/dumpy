db38e664eace63d783a1ca924b4ccbe1adca712c
Justin Li <justinnhli@gmail.com>
2025-05-17 12:42:21 -0700

    [ci] specify python executable for linting

--- ci/lint.sh
+++ ci/lint.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
-mypylint.py dumpy tests demos 2>&1 | grep -v 'no-name-in-module' | grep -v 'import-error'
+# set python3 executable
+PYTHON3=$HOME/.local/share/venv/dumpy/bin/python3
+$PYTHON3 $HOME/bin/mypylint.py dumpy tests demos 2>&1 | grep -v 'no-name-in-module' | grep -v 'import-error'
