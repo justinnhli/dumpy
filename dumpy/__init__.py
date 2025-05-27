3b493b3004e686c3d8f7e1edd50c84b19156f11a
Justin Li <justinnhli@gmail.com>
2025-05-27 12:06:55 -0700

    [dumpy] define module-level exports

--- dumpy/__init__.py
+++ dumpy/__init__.py
@@ -1,1 +1,39 @@
 """A dumpy of code that may eventually resemble a game engine."""
+
+from .algorithms import bentley_ottmann, monotone_triangulation
+from .animation import Animation, Sprite, Shape
+from .basic_window import BasicWindow
+from .camera import Camera
+from .canvas import Canvas, Input, EventCallback, Canvas
+from .color import Color
+from .data_structures import SortedDict, SortedSet, PriorityQueue
+from .game import Game
+from .game_object import GameObject
+from .matrix import Matrix, identity
+from .metaprogramming import CachedMetaclass
+from .polygon import Polygon
+from .scene import HierarchicalHashGrid
+from .simplex import Geometry, Point2D, Vector2D, Segment, Triangle
+from .transform import Transform
+from .transformable import Transformable
+
+
+__all__ = [
+    'bentley_ottmann', 'monotone_triangulation',
+    'monotone_triangulation',
+    'Animation', 'Sprite', 'Shape',
+    'BasicWindow',
+    'Camera',
+    'Canvas', 'Input', 'EventCallback', 'Canvas',
+    'Color',
+    'SortedDict', 'SortedSet', 'PriorityQueue',
+    'Game',
+    'GameObject',
+    'Matrix', 'identity',
+    'CachedMetaclass',
+    'Polygon',
+    'HierarchicalHashGrid',
+    'Geometry', 'Point2D', 'Vector2D', 'Segment', 'Triangle',
+    'Transform',
+    'Transformable',
+]
