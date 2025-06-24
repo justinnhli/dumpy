006bef71bb4c4be52d017d37de6caaed129adc2f
Justin Li <justinnhli@gmail.com>
2025-06-23 21:10:05 -0700

    [metaprogramming] remove cached_property
    functools.cached_property is much more performant

--- dumpy/metaprogramming.py
+++ dumpy/metaprogramming.py
@@ -2,7 +2,6 @@
 
 from collections import namedtuple
 from functools import lru_cache, wraps
-from typing import Any, Callable, Self
 
 
 def cached_class(cls, max_size=1024):
@@ -23,69 +22,3 @@ def cached_class(cls, max_size=1024):
     cls.__orig_new__ = cls.__new__
     cls.__new__ = __new__
     return cls
-
-
-CachedValue = namedtuple('CachedValue', 'cache, value')
-
-
-class cached_property: # pylint: disable = invalid-name
-    """Descriptor to cache properties of classes."""
-
-    def __init__(self, *upstream_attrs):
-        # type: (*str) -> None
-        if not all(isinstance(attr, str) for attr in upstream_attrs):
-            raise ValueError(' '.join([
-                'arguments to cached_property should be strs;',
-                'did you do @cached_property instead of @cached_property()?',
-            ]))
-        self.function = None # type: Callable[..., Any]
-        self.function_name = ''
-        self.upstream_attrs = upstream_attrs
-
-    def __call__(self, function):
-        # type: (Callable[..., Any]) -> Self
-        self.function = function
-        self.__doc__ = self.function.__doc__
-        self.function_name = self.function.__name__
-        return self
-
-    def __get__(self, obj, objtype=None):
-        # type: (Any, Any) -> Any
-        if obj is None:
-            return self
-        if not hasattr(obj, '_cached_properties'):
-            obj._cached_properties = {}
-        upstream_vals = self.collect_upstream_vals(obj)
-        cached_vals, value = obj._cached_properties.get(self.function_name, (None, None))
-        if cached_vals != upstream_vals:
-            value = self.function(obj)
-            obj._cached_properties[self.function_name] = CachedValue(
-                upstream_vals, value,
-            )
-        return value
-
-    def __set__(self, obj, value):
-        # type: (Any, Any) -> None
-        obj._cached_properties[self.function_name] = CachedValue(
-            self.collect_upstream_vals(obj),
-            value,
-        )
-
-    def __delete__(self, obj):
-        # type: (Any) -> None
-        raise AttributeError(' '.join([
-            f"cannot delete attribute '{self.function.__name__}'",
-            f"of '{obj.__class__.__name__}' objects",
-        ]))
-
-    def collect_upstream_vals(self, obj):
-        # type: (Any) -> tuple[Any, ...]
-        """Create a dictionary of upstream values."""
-        return tuple(getattr(obj, attr) for attr in self.upstream_attrs)
-
-    def is_cached(self, obj):
-        # type: (Any) -> bool
-        return (
-            hasattr(obj, '_cached_properties')
-            and self.function_name in obj._cached_properties
-        )
--- tests/metaprogramming_test.py
+++ tests/metaprogramming_test.py
@@ -1,9 +1,9 @@
 """Tests for metaprogramming.py."""
-# pylint: disable = comparison-with-callable, too-few-public-methods
 
+from functools import cached_property
 from typing import NamedTuple, Self
 
-from dumpy.metaprogramming import cached_class, cached_property
+from dumpy.metaprogramming import cached_class
 
 class _CachedClassDummy(NamedTuple):
     x: int
@@ -21,7 +21,7 @@ class CachedClassDummy(_CachedClassDummy):
         # type: (int) -> None
         self.num_calls = 0
 
-    @cached_property('x')
+    @cached_property
     def prop(self):
         # type: () -> str
         """Test property."""
@@ -46,88 +46,3 @@ def test_cached_class():
     assert x.num_calls == 1
     assert z.num_calls == 1
     assert w.num_calls == 0
-
-
-class CachedPropertyDummy:
-    """A dummy class to test cached_property."""
-
-    def __init__(self, value):
-        # type: (int) -> None
-        self.value = value
-        self.add_one_called = False
-        self.add_two_called = False
-
-    @cached_property('value')
-    def add_one(self):
-        # type: () -> int
-        """Add one to value."""
-        self.add_one_called = True
-        return self.value + 1
-
-    @cached_property('add_one')
-    def add_two(self):
-        # type: () -> int
-        """Add two to value."""
-        self.add_two_called = True
-        return self.add_one + 1
-
-
-def test_cached_property_basic():
-    # type: () -> None
-    """Test cached_property."""
-    obj1 = CachedPropertyDummy(1)
-    assert not obj1.add_one_called
-    assert obj1.add_one == 2, obj1.add_one
-    assert obj1.add_one_called
-    obj1.add_one_called = False
-    assert not obj1.add_one_called
-    assert obj1.add_one == 2
-    assert not obj1.add_one_called
-    obj1.add_one = 0
-    assert obj1.add_one == 0
-    assert not obj1.add_one_called
-    obj1.value = 2
-    assert obj1.value == 2
-    assert not obj1.add_one_called
-    assert obj1.add_one == 3
-    assert obj1.add_one_called
-    obj1.add_one_called = False
-    assert not obj1.add_one_called
-    assert obj1.add_one == 3
-    assert not obj1.add_one_called
-    obj2 = CachedPropertyDummy(4)
-    assert obj2.add_one == 5, obj2.add_one
-    assert obj2.add_one == 5, obj2.add_one
-
-
-def test_cached_property_chained():
-    # type: () -> None
-    """Test cached_property chains."""
-    obj = CachedPropertyDummy(10)
-    assert not obj.add_one_called
-    assert not obj.add_two_called
-    assert obj.add_one == 11
-    assert obj.add_one_called
-    assert not obj.add_two_called
-    obj.add_one_called = False
-    assert obj.add_one == 11
-    assert not obj.add_one_called
-    assert not obj.add_two_called
-    assert obj.add_two == 12
-    assert not obj.add_one_called
-    assert obj.add_two_called
-    obj.add_two_called = False
-    assert obj.add_two == 12
-    assert not obj.add_one_called
-    assert not obj.add_two_called
-    obj.value = 0
-    assert not obj.add_one_called
-    assert not obj.add_two_called
-    assert obj.add_two == 2
-    assert obj.add_one_called
-    assert obj.add_two_called
-    obj.add_one_called = False
-    obj.add_two_called = False
-    assert obj.add_one == 1
-    assert not obj.add_one_called
-    assert not obj.add_two_called
